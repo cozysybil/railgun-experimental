d984966c83cb2472832490ed6078de5113cbd28d
cozysybil <cozysybil@gmail.com>
2023-08-23 16:32:21 +0700

    feat: request token button

--- src/component/SubUserSection.tsx
+++ src/component/SubUserSection.tsx
@@ -12,8 +12,23 @@ const SubUserSection = (props: SubUserSectionProps) => {
   return (
     <div className="row-span-2 col-span-2 rounded flex flex-col p-1 gap-2 h-full overflow-auto">
       <div className="basis-1/12 break-words text-xs">Address: {address}</div>
-      <div className="basis-5/12 flex items-center text-4xl">
-        15 {privateSection ? "zkAA" : "AA"}
+      <div className="basis-5/12 flex flex-row items-stretch text-4xl gap-2">
+        <div className="py-2 px-0">
+          <span>15 {privateSection ? "zkAA" : "AA"}</span>
+        </div>
+        <div>
+          {!privateSection && (
+            <button
+              type="submit"
+              onClick={() => {
+                console.log("request AA");
+              }}
+              className="bg-violet-500 text-white text-sm font-bold py-1 px-2 rounded-full focus:outline-none focus:shadow-outline"
+            >
+              + request token
+            </button>
+          )}
+        </div>
       </div>
       <div className="basis-3/12 items-center ">
         <ActionWithAddress
@@ -30,7 +45,7 @@ const SubUserSection = (props: SubUserSectionProps) => {
           buttonContainerClassName={
             privateSection
               ? "basis-1/3 bg-gradient-to-r from-pink-500 to-violet-500 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              : "basis-1/3 bg-violet-500 to-violet-500 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              : "basis-1/3 bg-violet-500 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           }
           inputId={privateSection ? "unshieldTo" : "shieldTo"}
           inputPlaceHolder={
@@ -42,23 +57,23 @@ const SubUserSection = (props: SubUserSectionProps) => {
       </div>
       <div className="basis-3/12 items-center">
         <ActionWithAddress
-          buttonName={privateSection ? "Private Spend" : "Public Spend"}
+          buttonName={privateSection ? "Private Send" : "Public Send"}
           buttonAction={
             privateSection
               ? () => {
-                  console.log("Private spend");
+                  console.log("Private send");
                 }
               : () => {
-                  console.log("Public Spend");
+                  console.log("Public send");
                 }
           }
           buttonContainerClassName={
             privateSection
               ? "basis-1/3 bg-gradient-to-r from-pink-500 to-violet-500 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              : "basis-1/3 bg-violet-500 to-violet-500 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              : "basis-1/3 bg-violet-500 text-white text-xs font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           }
-          inputId={privateSection ? "privateSpendTo" : "publicSpendTo"}
-          inputPlaceHolder="Spend to address ..."
+          inputId={privateSection ? "privateSendTo" : "publicSendTo"}
+          inputPlaceHolder="Send to address ..."
           inputType="text"
           inputContainerClassName="grow"
         />
