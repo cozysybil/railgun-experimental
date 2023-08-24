1bc5d3024f354167818b66a01bce01f4befde42d
cozysybil <cozysybil@gmail.com>
2023-08-24 13:09:51 +0700

    build: use ethers v5

--- app/src/services/coin.ts
+++ app/src/services/coin.ts
@@ -311,18 +311,17 @@ export async function getBalance(address: string): Promise<string> {
     return "0";
   }
   try {
-    const provider = new ethers.JsonRpcProvider(rpc);
-
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    const signer = provider.getSigner();
     // Connect to the contract
-    const coinContract = new ethers.Contract(
+    const contractWithSigner = new ethers.Contract(
       contractAddress,
       contractABI,
-      provider
+      signer
     );
-
-    const balance = await coinContract.balanceOf(address);
+    const balance = await contractWithSigner.balanceOf(address);
     console.log("Balance:", balance.toString());
-    return balance.toString();
+    return ethers.utils.formatUnits(balance, 18);
   } catch (error) {
     console.error("Error fetching balance:", error);
     return "0";
@@ -332,12 +331,15 @@ export async function getBalance(address: string): Promise<string> {
 // Function to mint coins
 export async function mint(to: string, amount: string): Promise<void> {
   // Assuming the contract method name is "mint"
-  const provider = new ethers.JsonRpcProvider(rpc);
+  const provider = new ethers.providers.JsonRpcProvider(rpc);
+  const signer = provider.getSigner();
+  const contractWithSigner = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
 
-  const signer = await provider.getSigner();
-  const contractWithSigner = new Contract(contractAddress, contractABI, signer);
-
-  await contractWithSigner.mint(to, amount);
+  await contractWithSigner.mint(to, ethers.utils.parseUnits(amount, 18));
 }
 
 // // Function to transfer coins
@@ -351,24 +353,27 @@ export async function transfer(
     let contractWithSigner: ethers.Contract;
 
     if (metamask) {
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      const signer = await provider.getSigner();
-
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
       contractWithSigner = new ethers.Contract(
         contractAddress,
         contractABI,
         signer
       );
     } else {
-      const provider = new ethers.JsonRpcProvider(rpc);
-      const wallet = new ethers.Wallet(privateKey, provider);
+      const provider = new ethers.providers.JsonRpcProvider(rpc);
+      const signer = new ethers.Wallet(privateKey, provider);
       contractWithSigner = new ethers.Contract(
         contractAddress,
         contractABI,
-        wallet
+        signer
       );
     }
-    const receipt = await contractWithSigner.transfer(to, amount);
+    const receipt = await contractWithSigner.transfer(
+      to,
+      ethers.utils.parseUnits(amount, 18)
+    );
     return receipt.hash;
   } catch (error) {
     console.error("Error fetching balance:", error);
--- app/src/services/transaction.ts
+++ app/src/services/transaction.ts
@@ -2,7 +2,7 @@ import { ethers, Contract, Wallet } from "ethers";
 
 // Create a provider to connect to the Ethereum network
 const rpc = process.env.REACT_APP_RPC_URL || "";
-const provider = new ethers.JsonRpcProvider(rpc);
+const provider = new ethers.providers.JsonRpcProvider(rpc);
 
 export async function getTransactionDetails(
   transactionHash: string
